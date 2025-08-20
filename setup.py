626aa53720166bec0785c21951230a2cc450d44e
Alexandros Tzannes <alexandros.tzannes@aprioriinvestments.com>
2025-08-20 17:40:27 -0400

    Update how classifiers are declared
    Because I'm getting grief when installing
    
    Example of error:
    3.338   ERROR: Command errored out with exit status 1:
    3.338    command: /usr/bin/python /usr/local/lib/python3.10/dist-packages/pip/_vendor/pep517/in_process/_in_process.py get_requires_for_build_wheel /tmp/tmp_lcytom_
    3.338        cwd: /filesystem
    3.338   Complete output (96 lines):
    3.338   Could not find an up-to-date installation of `packaging`. License expressions might not be validated. To enforce validation, please install `packaging>=24.2`.
    3.338   /tmp/pip-build-env-nju_oomu/overlay/lib/python3.10/site-packages/setuptools/config/_apply_pyprojecttoml.py:75: _MissingDynamic: `classifiers` defined outside of `pyproject.toml` is ignored.
    3.338   !!
    3.338
    3.338           ********************************************************************************
    3.338           The following seems to be defined outside of `pyproject.toml`:
    3.338
    3.338           `classifiers = ['License :: OSI Approved :: Apache Software License', 'Programming Language :: Python :: 3 :: Only']`
    3.338
    3.338           According to the spec (see the link below), however, setuptools CANNOT
    3.338           consider this value unless `classifiers` is listed as `dynamic`.
    3.338
    3.338           https://packaging.python.org/en/latest/specifications/pyproject-toml/#declaring-project-metadata-the-project-table
    3.338
    3.338           To prevent this problem, you can list `classifiers` under `dynamic` or alternatively
    3.338           remove the `[project]` table from your file and rely entirely on other means of
    3.338           configuration.
    3.338           ********************************************************************************
    3.338
    3.338   !!
    3.338     _handle_missing_dynamic(dist, project_table)
    3.338   /tmp/pip-build-env-nju_oomu/overlay/lib/python3.10/site-packages/setuptools/config/expand.py:126: SetuptoolsWarning: File '/filesystem/README.md' cannot be found
    3.338     return '\n'.join(
    3.338   /tmp/pip-build-env-nju_oomu/overlay/lib/python3.10/site-packages/setuptools/config/_apply_pyprojecttoml.py:82: SetuptoolsDeprecationWarning: `project.license` as a TOML table is deprecated
    3.338   !!
    3.338
    3.338           ********************************************************************************
    3.338           Please use a simple string containing a SPDX expression for `project.license`. You can also use `project.license-files`. (Both options available on setuptools>=77.0.0).
    3.338
    3.338           By 2026-Feb-18, you need to update your project and remove deprecated calls
    3.338           or your builds will no longer be supported.
    3.338
    3.338           See https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license for details.
    3.338           ********************************************************************************
    3.338
    3.338   !!
    3.338     corresp(dist, value, root_dir)
    3.338   /tmp/pip-build-env-nju_oomu/overlay/lib/python3.10/site-packages/setuptools/config/_apply_pyprojecttoml.py:82: SetuptoolsWarning: `install_requires` overwritten in `pyproject.toml` (dependencies)

--- setup.py
+++ setup.py
@@ -1,31 +1,5 @@
 # Include setup.py to enable --editable installs.
+# All metadata is now defined in pyproject.toml
 import setuptools
 
-
-INSTALL_REQUIRES = [
-    "boto3",
-    "paramiko",
-    "pyftpdlib",
-    "pytz",
-]
-
-
-setuptools.setup(
-    name="filesystem",
-    version="1.0",
-    description=(
-        "FileSystem provides a basic file system abstraction "
-        "with multiple back-end implementations"
-    ),
-    author="Alexandros Tzannes and Braxton Mckee",
-    author_email="atzannes@gmail.com and braxton.mckee@gmail.com",
-    url="https://github.com/aprioriinvestments/filesystem",
-    packages=setuptools.find_packages(),
-    install_requires=INSTALL_REQUIRES,
-    # https://pypi.org/classifiers/
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3 :: Only",
-    ],
-    license="Apache Software License v2.0",
-)
+setuptools.setup()
